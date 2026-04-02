9152fff0c8323d98028bc05c550dc932eb3038fc
Luc Tielen <luc.tielen@lambdatech.be>
2026-04-02 09:17:51 +0200

    Fix github username

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://luctielen.github.io/koerswijziging',
+  site: 'https://luc-tielen.github.io/koerswijziging',
   base: '/koerswijziging',
   vite: {
     plugins: [tailwindcss()],
